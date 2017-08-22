219c421b97a7ca66bafb96ff8e4597d7126d9524
wuyl <wuyl@oriental-finance.com>
2017-08-22 16:15:34 +0800

    启动程序必须优先级最高才能加载其他包的程序

--- src/main/java/com/example/CloudserverApplication.java
+++ src/main/java/com/example/CloudserverApplication.java
@@ -1,12 +1,8 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableEurekaServer
